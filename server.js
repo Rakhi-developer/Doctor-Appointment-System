fd8b6848680f88a2f3c3858e205ad999b1044999
Rakhi-developer <145902659+Rakhi-developer@users.noreply.github.com>
2024-07-25 14:03:28 +0530

    final code
    deploy MERN Stack App

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const path=require("path")
+// const userRouters = require("./routes/userRouters.js");
+dotenv.config();
+
+// static files
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*',function (req,res) {
+  res.sendFile(path.join(__dirname,'./client/build/server.js'))
+})
+
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express();
+//middleware
+app.use(express.json());
+app.use(moragan("dev"));
+
+//routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+// port
+const port = process.env.port || 8080;
+//listen port
+app.listen(port, () => {
+  console.log(
+    `server Running in ${process.env.NODE_MODE}Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
